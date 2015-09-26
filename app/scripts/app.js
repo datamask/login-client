b421178d997366907befc7507c6c4c89730c0c14
datamask <datamasksoftware@gmail.com>
2015-09-27 00:28:37 +0530

    basic angular framework created

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name loginClientApp
+ * @description
+ * # loginClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('loginClientApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loginClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the loginClientApp
+ */
+angular.module('loginClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
